243b12a5927fd136d632b16196a516106bffc3eb
32003021 <110110789+32003021@users.noreply.github.com>
2022-10-18 19:39:53 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -3,6 +3,5 @@
 import streamlit as st
 from PIL import Image
 
-image01 = Image.open('desenvolvimento.jpg')
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
 st.title("MEU 1º WEB APP STREAMLIT")
