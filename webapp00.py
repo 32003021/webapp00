11bcfd7dfa9c9446c9c7f02405ed7a8d7a503072
32003021 <110110789+32003021@users.noreply.github.com>
2022-11-04 08:50:11 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -1,7 +1,43 @@
-# myFirstStreamlitApp.py
-#import the libraries
+"""
+# My first app
+Here's our first attempt at using data to create a table:
+"""
+
 import streamlit as st
+import numpy as np
+import pandas as pd
 from PIL import Image
 
-# Use st.title("") para adicionar um TÍTULO ao seu Web app
-st.title("MEU 1º WEB APP STREAMLIT")
+#Titulo
+st.title("AUTOCUST")
+
+#SubTitulo
+st.subheader('Quem se organiza economiza')
+
+#Introdução
+st.markdown('Nas ultimas décadas temos observado uma grande instabilidade politica, jurídica e econômica no país, esse fatores de instabilidade trouxeram uma alta oscilação de preços. No mundo dos automóveis não é diferente, as taxas vem se tornando cada vez mais elevadas e o preços dos combustíveis sofreu uma grande alta.')
+st.markdown('A AutoCust vem para ajudar os donos de carros a entender todos os custos com o combustível do seu veiculo e economizar muito. Vamos entender a composição dos combustíveis, comparar a rentabilidade entre gasolina e o etanol, alertar os clientes sobre o pagamento do IPVA, licenciamento e outras  datas importantes.')
+    
+image = Image.open('Autocust.jpg')
+st.image(image, caption='Autocust')
+
+# Add a selectbox to the sidebar:
+add_selectbox = st.sidebar.selectbox(
+    'Menu Principal',
+    ('Cadastro', 'Calculadora de Preços', 'Historico de Preços', 'Calendario')
+)
+
+# Add a slider to the sidebar:
+add_slider = st.sidebar.slider(
+    'Select a range of values',
+    0.0, 100.0, (25.0, 75.0)
+)
+
+
+#Mapa de Campinas
+map_data = pd.DataFrame(
+    np.random.randn(500, 2) / [50, 50] + [-22.90556, -47.06083],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
+  
