4d6adc72b2833448aa6872ce9ac5a078bd47a24f
32003021 <110110789+32003021@users.noreply.github.com>
2022-11-18 08:09:33 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -6,21 +6,12 @@ import streamlit as st
 
 This is some _markdown_.
 '''
+st.title("Autocust")
 
-import pandas as pd
-df = pd.DataFrame({'col1': [1,2,3]})
-df  # 👈 Draw the dataframe
-
-x = 10
-'x', x  # 👈 Draw the string 'x' and then the value of x
-
-# Also works with most supported chart types
-import matplotlib.pyplot as plt
-import numpy as np
-
-arr = np.random.normal(1, 1, size=100)
-fig, ax = plt.subplots()
-ax.hist(arr, bins=20)
-
-fig  # 👈 Draw a Matplotlib chart
+[theme]
+primaryColor="#F63366"
+backgroundColor="#FFFFFF"
+secondaryBackgroundColor="#F0F2F6"
+textColor="#262730"
+font="sans serif"
 
